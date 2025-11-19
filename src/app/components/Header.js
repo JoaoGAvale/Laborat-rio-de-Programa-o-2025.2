8a66968e37cf7ae5c7966da8158dc4d14a0be4f8
JoaoGAvale <jga.vale@discente.ufma.br>
2025-11-18 22:56:38 -0300

    [João Vale] fix: Colocando o nome do site na lateral da sidebar.

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -7,10 +7,12 @@ import { Menu } from "lucide-react";
 export default function Header({ onOpen }) {
   return (
     <header className="w-full fixed z-[1000] bg-green-400 text-gray-800 px-6 py-4 flex items-center justify-between shadow-md font-['PoppinsRegular']">
-      <button onClick={onOpen} className="p-2">
-        <Menu />
-      </button>
-      <h1 className="text-2xl font-bold text-nowrap">Prato Solidário</h1>
+      <div className="flex items-center gap-6">
+        <button onClick={onOpen} className="p-2">
+          <Menu />
+        </button>
+        <h1 className="text-2xl font-bold text-nowrap">Prato Solidário</h1>
+      </div>
 
       <nav className="flex items-center gap-6">
         <a href="/" className="hover:text-green-900 transition">Início</a>
